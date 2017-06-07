3196d6c88d26c422d0f8114e2475dc599ad7dfc1
aleix <aleix11fcb@gmail.com>
2017-06-07 15:04:55 +0200

    Actualització: Mapa eetakemons recividos por servidor

---
+++ app/src/main/java/com/example/aleix/myapplication/Entity/Location.java
@@ -0,0 +1,36 @@
+package com.example.aleix.myapplication.Entity;
+
+public class Location {
+
+    private double latitud;
+    private double longitud;
+
+    public Location(){}
+
+    public Location(double latitud, double longitud)
+    {
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    @Override
+    public String toString() {
+        return "Location [latitud=" + latitud + ", longitud=" + longitud + "]";
+    }
+}
